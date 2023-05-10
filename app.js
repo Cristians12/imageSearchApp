097cecbb0fd873f9090e9bcbcff2543adbd47d83
Cristians12 <cristi_s007@yahoo.ro>
2023-05-10 22:27:35 +0300

    img search

---
+++ app.js
@@ -0,0 +1,52 @@
+const accsKey = "pYH9zU_gMtckmCHRZNBt8Fuwh6hq64OAJyKXhYEPfXo";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultEl = document.querySelector(".search-results");
+const showMoreBtnEl = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = searchInputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accsKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  if (page === 1) {
+    searchInputEl.innerHTML = "";
+  }
+
+  const results = data.results;
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const img = document.createElement("img");
+    img.src = result.urls.small;
+    img.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(img);
+    imageWrapper.appendChild(imageLink);
+    searchResultEl.appendChild(imageWrapper);
+  });
+
+  page++;
+
+  if (page > 1) {
+    showMoreBtnEl.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
+showMoreBtnEl.addEventListener("click", () => {
+  searchImages();
+});
